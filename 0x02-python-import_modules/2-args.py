5bcf97c4ac7e8133abec47edd75bb34fe4308838
George Israel <georgeisrael18@gmail.com>
2023-08-11 15:38:10 -0700

    import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,6 @@
 import sys
 
 def main(*argv):
-
     i = 0
     l = len(sys.argv) - 1
     if l == 1:
@@ -11,11 +10,10 @@ def main(*argv):
         print("{:d} arguments.".format(l))
     else:
         print("{:d} arguments:".format(l))
-   
-   for args in sys.argv:
-        if (i != 0):
-            print("{}: {}".format(i, args))
-        i += 1
+        for args in sys.argv:
+            if (i != 0):
+                print("{}: {}".format(i, args))
+                i += 1
 
-if __name__ == "__main__":
-    main()
+                if __name__ == "__main__":
+                    main()
