5132b970770ecc200748d1e12971d2fce36a3656
George Israel <georgeisrael18@gmail.com>
2023-09-12 19:23:56 -0700

    python-input_output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that writes a string to a text file (UTF8)
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)"""
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
