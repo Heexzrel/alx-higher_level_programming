dd86d006c65d069b40a487433f4666425a83b9cd
George Israel <georgeisrael18@outlook.com>
2023-10-18 19:04:45 -0700

    more_queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create a datbase and user with Select only privlage
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- create a unique_id table 
+
+CREATE TABLE IF NOT EXISTS unique_id (id int DEFAULT 1 UNIQUE, name varchar(256));
